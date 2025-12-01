701435bdde72293700924068c85832292ee51430
Matt <fabianbarzola2003@gmail.com>
2025-12-01 19:01:43 -0300

    Update Task.ts

--- Task.ts
+++ Task.ts
@@ -1,13 +1,26 @@
 export class Task {
-    title: string;
-    description: string;
-    difficulty: number;
-    status: number;
-
-    constructor(title: string, description: string, difficulty: number, status: number) {
-        this.title = title;
-        this.description = description;
-        this.difficulty = difficulty;
-        this.status = status;
-    }
+  title: string;
+  description: string;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  dueDate: string;
+  difficulty: number;
+  constructor(
+    title: string,
+    description: string,
+    status: string,
+    createdAt: string,
+    updatedAt: string,
+    dueDate: string,
+    difficulty: number
+  ) {
+    this.title = title;
+    this.description = description;
+    this.status = status;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.dueDate = dueDate;
+    this.difficulty = difficulty;
+  }
 }
