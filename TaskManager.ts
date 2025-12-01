a9add3c85f30d51d74487b82b191f2a8acf98bbe
Matt <fabianbarzola2003@gmail.com>
2025-12-01 20:12:59 -0300

    feat: add showstats function and fixes

--- TaskManager.ts
+++ TaskManager.ts
@@ -1,5 +1,5 @@
-import { Task } from './Task';
-import tasksJson from './tasks.json';
+import { Task } from './Task.ts';
+import tasksJson from './tasks.json' with { type: 'json' };
 export class TaskManager {
   tasks: Task[] = tasksJson as Task[];
   //equisde
